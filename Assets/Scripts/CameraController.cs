c8b0f56cceec6aefa7cd33425de7b1a0a7f1ad2e
joeycarlos <jclaguio@gmail.com>
2020-03-04 20:20:02 -0800

    Init repository

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 2.0f;
+    public Vector3 offsetVector;
+
+    void Start() {
+        transform.position = target.transform.position + offsetVector;
+    }
+
+    void LateUpdate() {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = transform.position;
+        position.x = Mathf.Lerp(transform.position.x, target.transform.position.x + offsetVector.x, interpolation);
+        position.y = Mathf.Lerp(transform.position.y, target.transform.position.y + offsetVector.y, interpolation);
+        position.z = offsetVector.z;
+
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private float horizontalMovement;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ReadInput();
+        if (horizontalMovement != 0) Move();
+    }
+
+    void ReadInput() {
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed;
+    }
+
+    void Move() {
+        Vector3 moveVector = new Vector3(horizontalMovement * Time.deltaTime, 0, 0);
+        transform.Translate(moveVector);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Memory")) {
+            Destroy(collision.gameObject);
+        }
+    }
+}

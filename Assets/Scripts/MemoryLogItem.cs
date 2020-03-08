9188d8ac7854949a9aa2388fd45add06056d6b40
joeycarlos <jclaguio@gmail.com>
2020-03-08 01:11:37 -0800

    Add animations folder and adjust question mark icon

--- Assets/Scripts/MemoryLogItem.cs
+++ Assets/Scripts/MemoryLogItem.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MemoryLogItem : MonoBehaviour, ISelectHandler {
 
     public int id;
+
+    public void Start() {
+        Image mImage = GetComponentsInChildren<Image>()[1];
+
+        MemoryData mData = MemoryManager.Instance.memoryDataList[id];
+        if (id < 24 && id < GameManager.Instance.memoriesUnlocked) {
+            mImage.sprite = mData.sprite;
+        }
+        else {
+            mImage.sprite = MemoryManager.Instance.memoryDataList[24].sprite;
+        }
+    }
 
     public void OnSelect(BaseEventData eventData) {
         // Update featured memory log item
